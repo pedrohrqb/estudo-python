475d25c97c3ad3c9b41b84a0eb5aaf41dcccd353
Pedro Henrique <140624712+pedrohrqb@users.noreply.github.com>
2026-04-29 20:42:39 -0300

    att

--- def046.py
+++ def046.py
@@ -1,5 +1,6 @@
 from time import sleep
 
+#Usando laço for para fazer uma contagem regressiva com intervalo de tempo de 1seg.
 for cont in range(0, 11, 1):
     sleep(1)
     print(cont)
--- def047.py
+++ def047.py
@@ -1,4 +1,5 @@
 
+#Usando o laço for para fazer uma contagem dos números pares entre 1 e 50
 for n in range(1, 50):
     if n % 2 == 0:
         print(f'{n} ', end='')
